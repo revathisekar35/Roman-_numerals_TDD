bad4ae5d2d57d957a1ffa68a348639426d9630a9
revathisekar35 <revathisekar2010@gmail.com>
2022-01-21 11:06:58 +0000

    Added new test cases for Roman numerals

--- src/test/java/com/techreturners/romannumerals/service/RomanServiceImplTest.java
+++ src/test/java/com/techreturners/romannumerals/service/RomanServiceImplTest.java
@@ -15,13 +15,26 @@ public class RomanServiceImplTest {
 	public void getRomanNumberTest(RomanNumber romanNumber) {
 		RomanServiceImpl romanService = new RomanServiceImpl();
 		assertEquals(romanNumber.romanNumber, romanService.getRomanNumber(romanNumber.numbericalNumber));
+		assertEquals(romanNumber.romanNumber, romanService.getRomanNumber(romanNumber.numbericalNumber));
 	}
 
 	@Test
-	public void getRomanNumberFor0and3001Test() {
+	public void getRomanNumberForOutOfRangeTest() {
 		RomanServiceImpl romanService = new RomanServiceImpl();
 		assertEquals("Accepted value range : 1 - 3000.", romanService.getRomanNumber(0));
 		assertEquals("Accepted value range : 1 - 3000.", romanService.getRomanNumber(3001));
+		assertEquals("Accepted value range : 1 - 3000.", romanService.getRomanNumber(-8));
+	}
+
+	@Test
+	public void getRomanNumberForRandomNumberTest() {
+		RomanServiceImpl romanService = new RomanServiceImpl();
+		assertEquals("CMXCIX", romanService.getRomanNumber(999));
+		assertEquals("MMM", romanService.getRomanNumber(3000));
+		assertEquals("LXX", romanService.getRomanNumber(70));
+		assertEquals("CDXCIX", romanService.getRomanNumber(499));
+		assertEquals("MMD", romanService.getRomanNumber(2500));
+		assertEquals("MMCMXCIX", romanService.getRomanNumber(2999));
 	}
 
 }
