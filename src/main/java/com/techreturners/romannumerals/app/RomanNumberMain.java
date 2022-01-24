85ac81007e89f2c2ead5c176a59dcad9c993b3f3
revathisekar35 <revathisekar2010@gmail.com>
2022-01-24 02:07:11 +0000

    updated main class

--- src/main/java/com/techreturners/romannumerals/app/RomanNumberMain.java
+++ src/main/java/com/techreturners/romannumerals/app/RomanNumberMain.java
@@ -1,10 +1,15 @@
 package com.techreturners.romannumerals.app;
 
+import com.techreturners.romannumerals.service.RomanServiceImpl;
+
 public class RomanNumberMain {
 
-	public static void main(String[] args) {
-		System.out.println("==============");
-
+	public static void main(String[] args) throws Exception {
+		RomanServiceImpl rom = new RomanServiceImpl();
+		System.out.println("Convert 440 into roman number:\n");
+		System.out.println(rom.getRomanNumber(440));
+		System.out.println("\nConvert CDXL into numerical number:\n");
+		System.out.println(rom.getNumericalNumberFormRomanNumber("CDXL"));
 	}
 
 }
