06bb2db95a12318c6c87a28e3a130355146e4591
revathisekar35 <revathisekar2010@gmail.com>
2022-02-05 17:35:56 +0000

    Added interface method

--- src/main/java/com/techreturners/romannumerals/service/RomanService.java
+++ src/main/java/com/techreturners/romannumerals/service/RomanService.java
@@ -1,5 +1,6 @@
 package com.techreturners.romannumerals.service;
 
 public interface RomanService {
-
+	int getNumericalNumberFormRomanNumber(String roamNumber) throws Exception;
+	String getRomanNumber(int number);
 }
